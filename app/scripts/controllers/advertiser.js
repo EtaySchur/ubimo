92ca6c0e51cc77aab90700ffaa054a978ccbe346
Etay <etayschur@gmail.com>
2016-04-03 16:08:34 +0300

    clusers

--- app/scripts/controllers/advertiser.js
+++ app/scripts/controllers/advertiser.js
@@ -11,62 +11,68 @@
     .controller('AdvertiserCtrl', AdvertiserCtrl);
 
   function AdvertiserCtrl( $scope , advertisersService , $routeParams , notificationService , $location , $q ) {
-    var advertiserId = $routeParams.advertiserId;
+    var advertiser = {
+        _init : function(){
+            var advertiserId = $routeParams.advertiserId;
 
-    // Check if advertiser id was passed to controller ( case edit exist advertiser )
-    if(advertiserId){
-        advertisersService.getAdvertiser(advertiserId).then(function (result) {
-            $scope.advertiser = result;
-            $scope.$emit('AddAdvertiserNav', {data: $scope.advertiser});
-        }).catch(function(err){
-            $location.path('/');
-            notificationService.error("Advertiser Not Found");
+            // Check if advertiser id was passed to controller ( case edit exist advertiser )
+            if(advertiserId){
+                advertisersService.getAdvertiser(advertiserId).then(function (result) {
+                    $scope.advertiser = result;
+                    $scope.$emit('AddAdvertiserNav', {data: $scope.advertiser});
+                }).catch(function(err){
+                    $location.path('/');
+                    notificationService.error("Advertiser Not Found");
 
-        });
-    // Case New Advertiser
-    }else{
-        $scope.advertiser = {};
+                });
+                // Case New Advertiser
+            }else{
+                $scope.advertiser = {};
+            }
+
+
+            // New Advertiser Buttons Group Used By Actions Bar Component
+            $scope.newAdvertiserActionsBar = [
+                {
+                    text : 'Save New Advertiser',
+                    class : 'success' ,
+                    callbackFunc :  saveNewAdvertiser ,
+                    isDisabled : true
+
+                },
+                {
+                    text : 'Cancel',
+                    class : 'danger' ,
+                    callbackFunc :  cancel
+                }
+            ];
+
+            // Edit Advertiser Buttons Group Used By Actions Bar Component
+            $scope.editAdvertiserActionsBar = [
+                {
+                    text : 'Save',
+                    class : 'success' ,
+                    callbackFunc :  editAdvertiser ,
+                    isDisabled : true
+
+                },
+                {
+                    text : 'Save & Close',
+                    class : 'success' ,
+                    callbackFunc :  editAdvertiserAndClose ,
+                    isDisabled : true
+
+                },
+                {
+                    text : 'Cancel',
+                    class : 'danger' ,
+                    callbackFunc :  cancel
+                }
+            ];
+        }
     }
 
-
-    // New Advertiser Buttons Group Used By Actions Bar Component
-    $scope.newAdvertiserActionsBar = [
-      {
-        text : 'Save New Advertiser',
-        class : 'success' ,
-        callbackFunc :  saveNewAdvertiser ,
-        isDisabled : true
-
-      },
-      {
-        text : 'Cancel',
-        class : 'danger' ,
-        callbackFunc :  cancel
-      }
-    ];
-
-    // Edit Advertiser Buttons Group Used By Actions Bar Component
-    $scope.editAdvertiserActionsBar = [
-      {
-        text : 'Save',
-        class : 'success' ,
-        callbackFunc :  editAdvertiser ,
-        isDisabled : true
-
-      },
-      {
-        text : 'Save & Close',
-        class : 'success' ,
-        callbackFunc :  editAdvertiserAndClose ,
-        isDisabled : true
-
-      },
-      {
-        text : 'Cancel',
-        class : 'danger' ,
-        callbackFunc :  cancel
-      }
-    ];
+      advertiser._init();
 
 
 
